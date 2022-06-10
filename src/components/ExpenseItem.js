ebd0de6263122a934b9707b84a749b68f32179cd
Ryan Cabanas <ryan@Ryans-MacBook-Pro-14-M1-Pro.local>
2022-06-10 00:23:28 -0400

    Lessons 29 - 34

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem() {
+  return (<div>
+    <div>March 28, 2022</div>
+    <div>
+      <h2>Car Insurance</h2>
+      <div>$249.67</div>
+    </div>
+  </div>
+  );
+}
+
+export default ExpenseItem;

bbf14712cbece6ba21e28c67f2a073fa41dd8986
Ryan Cabanas <ryan@Ryans-MacBook-Pro-14-M1-Pro.local>
2022-06-13 23:54:05 -0400

    Up to Lesson 39, including the assignment.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,15 @@
-function ExpenseItem() {
-  return (<div>
-    <div>March 28, 2022</div>
-    <div>
-      <h2>Car Insurance</h2>
-      <div>$249.67</div>
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
     </div>
-  </div>
   );
 }
 

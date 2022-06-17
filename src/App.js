7064e08b7b67e7105cd6eec3e8f8c1950231d89a
Ryan Cabanas <ryan@Ryans-MacBook-Pro-14-M1-Pro.local>
2022-06-16 22:23:07 -0400

    - Update `ExpenseItem` component:
      - Implement the `useState` function from the `React` module.  `useState` is a
        key concept pertaining to updating components on the page.  We implement
        the `useState` function for the *"title"* variable coming in via the `props`
        parameter.
    
      - We also add a `button` element that responds to the `onClick` event.  All
        ***Element*** events being with `on`.
    
    - Add `NewExpense` and `ExpenseForm` components.
    
      - `NewExpense` is really just a wrapper component.  It will hold the
        `ExpenseForm` component.
    
      - `ExpenseForm` component contains the input elements for creating a new
        expense item, which isn't completely hooked up yet, but the `<form>` shell
        is there now.
    
    - Update the `App` component.
    
      - We add the `NewExpense` wrapper component to the top of the page.  This
        `NewExpense` component holds the `ExpenseForm` component.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React from "react";
 
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
   const expenses = [
@@ -28,7 +29,7 @@ const App = () => {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses expenses={expenses} />
     </div>
   );
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,24 @@
+import { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle("Updated!!");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-    </Card>
+      <button onClick={clickHandler}>Change Title</button>
+    </Card >
   );
 };
 

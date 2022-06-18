cee1b83be4b69aba3f2d0d79dcdb408fe4daddde
Ryan Cabanas <ryan@Ryans-MacBook-Pro-14-M1-Pro.local>
2022-06-17 22:30:29 -0400

    Topics: - Listening to user input
      - Here we implement the `onChange` event listener to each of our `input`
        elements.  These will call handler functions we've created.
    
    - Working with multiple states
    
      - Since we want these event handlers to set and save the state for these
        `input` elements, we need to create these states and them in our event
        handler functions.
    
    - Alternate approach - Using a single state object vs. multiple states
    
      - In the commented out code, you will see the creation of a single state whose
        starting value is an object with properties representing our states.  This
        is an alternative method of handling multiple states.
    
    - Updating a state that depends on previous states (when using a single state
      object)
    
      - In the commented code of the event handlers, you'll see that the
        `setUserInput` function (also known as the ***setState*** function) is being
        passed another function, whose argument is named `prevState`.  If you
        plan to handle multiple states in a single state object like this, when
        updating one of the states in this state object, you have to handle updates
        this way because the task of updating a state value is essentiall
        asynchronous.  If you don't do it this way, you run the risk of having the
        newer state overwritten by the older state.  Using this approach (as
        documented in the React docs) basically makes the update process
        synchronous.  Thus, the `prevState` parameter (with the spread operator `...`)
        is written to the state object first and ***then*** the new state writes
        its value.  Thus keeping the write ordering correct for all states in the
        object.
    
    - Handling form submissions
    
      - Instead of adding an event listener to the `button` element, the *"correct"*
        way to submit a form is to use the `onSubmit` event listener on the `form`
        element.  Along with this we create a `submitHandler` function.  In this
        handler function, we do two things:
    
        - We pass in the `event` argument, which is available to us by default, and
          we call its `preventDefault` method (which is a vanilla JS method).  If
          you don't call this method, then the form will relead upon submit.  We
          don't want this to happen.  This is why we call this method.
    
        - The other thing we do in this handler function is save the state values
          in a JS object.
    
    - Two-way binding
    
      - In the `submitHandler` function, we do a third thing.  We reset the state
        values back to their default values ***after*** saving the state values
        in our JS object.  This is done by calling the `setState` *(generic name
        reference)* functions for each of the states and setting them back to their
        starting (or default) values.  (Here I created some `default...` constants
        that can be reused with each `setState` function.)
    
        In addition to using the `setState` functions to reset the state values,
        though, we need to do one more thing to get these values to reflect on the
        page.  Initially, all we cared about was listening for input via event
        listeners.  But to write ***back*** to the elements, we need to take our
        state values and set them using the `value` attribute on each of our `input`
        elements.  And this is how ***we*** write values back to the `input` elements
        on the page.

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,20 +1,78 @@
+import { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+  // Set some default state values.
+  const defaultTitle = "";
+  const defaultAmount = "";
+  const defaultDate = "";
+
+  // Create states
+  const [enteredTitle, setEnteredTitle] = useState(defaultTitle);
+  const [enteredAmount, setEnteredAmount] = useState(defaultAmount);
+  const [enteredDate, setEnteredDate] = useState(defaultDate);
+
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: "",
+  //   enteredAmount: "",
+  //   enteredDate: ""
+  // });
+
+  const titleChangeHandler = event => {
+    setEnteredTitle(event.target.value);
+
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredTitle: event.target.value };
+    // });
+  };
+
+  const amountChangeHandler = event => {
+    setEnteredAmount(event.target.value);
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredAmount: event.target.value };
+    // });
+  };
+
+  const dateChangeHandler = event => {
+    setEnteredDate(event.target.value);
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredDate: event.target.value };
+    // });
+  };
+
+  const submitHandler = (event) => {
+    // Prevent form from reloading on "Submit"
+    event.preventDefault();
+
+    // Store state values in an object.
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate)
+    };
+
+    console.log(expenseData);
+
+    // Reset states back to default values after "Submit"
+    setEnteredTitle(defaultTitle);
+    setEnteredAmount(defaultAmount);
+    setEnteredDate(defaultDate);
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input type="number" min="0.01" step="0.01" value={enteredAmount} onChange={amountChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" />
+          <input type="date" min="2019-01-01" max="2022-12-31" value={enteredDate} onChange={dateChangeHandler} />
         </div>
       </div>
       <div className="new-epense__actions">

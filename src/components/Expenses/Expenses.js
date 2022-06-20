bc205e1d1d4ad32c8beef8e01f4324ad3b7dcab5
Ryan Cabanas <ryan@Ryans-MacBook-Pro-14-M1-Pro.local>
2022-06-20 15:47:06 -0400

    67. Outputting Conditional Content
    - We want to either show a list of expenses for the chosen year, or if there are
      none for the chosen year, show a "No expenses found" message.
    
      We can accomplish this by creating an `expenseContent` variable outside of the
      `return` and setting the variable to either this "No expenses found" message,
      or setting it equal to the filtered and mapped list of expenses.  Then, in the
      end, we simply add a dynamic expression to the returned component that provides
      whatever is in the `expenseContent` variable.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -14,18 +14,23 @@ const Expenses = (props) => {
 
   const filteredExpenses = props.items.filter(expense => expense.date.getFullYear().toString() === filteredYear);
 
+  let expensesContent = <p>No expenses found</p>;
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map(expense => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
+
   return (
     <div>
       <Card className='expenses'>
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        {filteredExpenses.map(expense => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {expensesContent}
       </Card>
     </div>
   );

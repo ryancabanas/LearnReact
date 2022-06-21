4d87750e1221b1a025d7db1c56d61bf85b9a93ad
Ryan Cabanas <ryangithub777@gmail.com>
2022-06-21 19:55:55 -0400

    SECTION 5 - Assignment 4: Time to Practice: Conditional Content (Ryan's Approach)
    - I did this assignment differently than the instructor.  I will be updating my
      code to his after this commit.
    
    - The goal was to utilize conditionals to change what showed at the top of the
      page: either a button to start adding a new expense, or the new expense form.
      For my approach, I ended up putting the conditional inside of the `ExpenseForm.js`
      component, but I can see why my approach was not the best approach because it
      *taints* the `ExpenseForm.js` component.  It's no longer a pur Expense Form
      component now.  Now, it's conditionally an Expense Form component, or just
      a button to get to the *real* Expense Form component.
    
      Therefore, the instructor's approach was to modify the `NewExpense.js`
      component moreso than the `ExpenseForm.js` component (which still needed to
      be modified some).  The `NewExpense.js` was the more appropriate place to put
      the new button and the primary logic to control what is shown (the single
      button, or the Expense Form).
    
    - Note that because I put the state value in the `ExpenseForm.js` component, if
      you add some text to the form fields and don't submit them, but click the
      **"Cancel"** button, if you click the **"Add New Expense"** button again, the
      previous values you entered will still be waiting for you in these fields.
      I believe this is because I'm holding the state of the form here, essentially.
      The instructor's version, where the state is kept at the `NewExpense.js`
      component, doesn't behave this way.

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -6,11 +6,7 @@ const ExpenseForm = (props) => {
   const [enteredTitle, setEnteredTitle] = useState('');
   const [enteredAmount, setEnteredAmount] = useState('');
   const [enteredDate, setEnteredDate] = useState('');
-  // const [userInput, setUserInput] = useState({
-  //   enteredTitle: '',
-  //   enteredAmount: '',
-  //   enteredDate: '',
-  // });
+  const [displayExpenseForm, setDisplayExpenseForm] = useState(false);
 
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
@@ -52,45 +48,70 @@ const ExpenseForm = (props) => {
     setEnteredTitle('');
     setEnteredAmount('');
     setEnteredDate('');
+
+    closeNewExpenseHandler();
   };
 
-  return (
-    <form onSubmit={submitHandler}>
-      <div className='new-expense__controls'>
-        <div className='new-expense__control'>
-          <label>Title</label>
-          <input
-            type='text'
-            value={enteredTitle}
-            onChange={titleChangeHandler}
-          />
+
+  // Code for New Expense display
+  const addNewExpenseHandler = () => {
+    setDisplayExpenseForm(true);
+  };
+
+  const closeNewExpenseHandler = () => {
+    setDisplayExpenseForm(false);
+  };
+
+
+  if (displayExpenseForm) {
+    return (
+      <form onSubmit={submitHandler}>
+        <div className='new-expense__controls'>
+          <div className='new-expense__control'>
+            <label>Title</label>
+            <input
+              type='text'
+              value={enteredTitle}
+              onChange={titleChangeHandler}
+            />
+          </div>
+          <div className='new-expense__control'>
+            <label>Amount</label>
+            <input
+              type='number'
+              min='0.01'
+              step='0.01'
+              value={enteredAmount}
+              onChange={amountChangeHandler}
+            />
+          </div>
+          <div className='new-expense__control'>
+            <label>Date</label>
+            <input
+              type='date'
+              min='2019-01-01'
+              max='2022-12-31'
+              value={enteredDate}
+              onChange={dateChangeHandler}
+            />
+          </div>
         </div>
-        <div className='new-expense__control'>
-          <label>Amount</label>
-          <input
-            type='number'
-            min='0.01'
-            step='0.01'
-            value={enteredAmount}
-            onChange={amountChangeHandler}
-          />
+        <div className='new-expense__actions'>
+          <button type='button' onClick={closeNewExpenseHandler}>Cancel</button>
+          <button type='submit'>Add Expense</button>
         </div>
-        <div className='new-expense__control'>
-          <label>Date</label>
-          <input
-            type='date'
-            min='2019-01-01'
-            max='2022-12-31'
-            value={enteredDate}
-            onChange={dateChangeHandler}
-          />
+      </form>
+    );
+  }
+  else {
+    return (
+      <form>
+        <div className='new-expense__add-new-expense'>
+          <button type='button' onClick={addNewExpenseHandler}>Add New Expense</button>
         </div>
-      </div>
-      <div className='new-expense__actions'>
-        <button type='submit'>Add Expense</button>
-      </div>
-    </form>
-  );
+      </form>
+    );
+  }
 };
 
 export default ExpenseForm;

375c27cae02e0d8d7946cbb5e3fca161563e5167
Ryan Cabanas <ryangithub777@gmail.com>
2022-06-21 17:47:31 -0400

    Fix: Change `classname` to `className` in `ExpenseList.js` component.

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -4,7 +4,7 @@ import ExpenseItem from './ExpenseItem';
 const ExpensesList = (props) => {
   if (props.items.length === 0) {
     return (
-      <h2 classname="expenses-list__fallback">Found no expenses.</h2>
+      <h2 className="expenses-list__fallback">Found no expenses.</h2>
     );
   }
 

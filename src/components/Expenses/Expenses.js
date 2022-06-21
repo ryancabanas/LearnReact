914d34d5394f71d0ae030ef0b6cb2f5d89d05006
Ryan Cabanas <ryangithub777@gmail.com>
2022-06-20 23:25:34 -0400

    68. Adding Conditional Return Statements
    - Primary goal was to break apart the `Expenses.js` component a little bit more
      and put the list of expenses in a separate component called `ExpensesList.js`.
    
    - The list filtering logic remains in the `Expenses.js` component, since the
      dropdown year is there too and is needed to do the filtering, but the logic
      for the conditional expense list display has been move to the new component.
    
    - In the new `ExpensesList.js` component, we handle the conditional list display
      by using alternative `return` statements, which is a little different from how
      it was being done in the `Expenses.js` component.  In one case, we return
      an unordered list, and the other we return a "no expenses found" message.
    
    - Since an `<ul>` element is used for the list, we needed to also change the
      `ExpenseItem.js` component (that builds the individual expenses for the list)
      to wrap each component item in a `<li>` element.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
 import './Expenses.css';
 
 const Expenses = (props) => {
@@ -14,23 +14,11 @@ const Expenses = (props) => {
 
   const filteredExpenses = props.items.filter(expense => expense.date.getFullYear().toString() === filteredYear);
 
-  let expensesContent = <p>No expenses found</p>;
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map(expense => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
-
   return (
     <div>
       <Card className='expenses'>
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        {expensesContent}
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );

cee1c6733970522ff5b9c023e02f428e6d091b88
Ryan Cabanas <ryangithub777@gmail.com>
2022-06-21 23:40:13 -0400

    Fix: Despite the auto-formatting that's also present, the only change we're really interested in is the one on Line 47 where we add the `+` to the beginning of the `enteredAmount` state value.  We do this because, otherwise, `enteredAmount` would return a string.  Adding a `+` is a shorthand way of converting a string number to a real number.  (If you were to add a `-` in front of the variable, you'd get the negative of the number.  If the variable wasn't a string number, you'd get `NaN` back.)

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -44,8 +44,8 @@ const ExpenseForm = (props) => {
 
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
-      date: new Date(enteredDate),
+      amount: +enteredAmount,
+      date: new Date(enteredDate)
     };
 
     props.onSaveExpenseData(expenseData);
@@ -87,7 +87,9 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className='new-expense__actions'>
-        <button type="button" onClick={props.onCancel}>Cancel</button>
+        <button type='button' onClick={props.onCancel}>
+          Cancel
+        </button>
         <button type='submit'>Add Expense</button>
       </div>
     </form>
